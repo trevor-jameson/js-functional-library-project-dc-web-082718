94180328f1f8c5076a094a6f49af9b4a822de89a
Trevor Jameson <github email address>
2018-10-09 14:00:38 -0400

    3 functions completed

--- index.js
+++ index.js
@@ -4,18 +4,72 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+      for (const elem in collection) {
+        callback(collection[elem])
+      }
+      return collection
+    },
+
+    map: function(array, callback) {
+      let newArr = []
+      for (const elem in array) {
+        newArr.push(callback(array[elem]))
+      }
+      return newArr
+    },
+
+    reduce: function(collection, callback, acc) {
+
+      if (arguments.length < 3) {
+        acc = 0
+      }
+      for (const elem in collection) {
+        acc = callback(acc, collection[elem], collection)
+      }
+      return acc
+    },
+
+    find: function() {
 
     },
 
-    map: function() {
+    functions: function() {
 
     },
 
-    reduce: function() {
+    functions: function() {
 
     },
-    
+
+    functions: function() {
+
+    },
+
+    functions: function() {
+
+    },
+
+    functions: function() {
+
+    },
+
+    functions: function() {
+
+    },
+
+    functions: function() {
+
+    },
+
+    functions: function() {
+
+    },
+
+    functions: function() {
+
+    },
+
     functions: function() {
 
     },
